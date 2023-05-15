4e3e524cf528a48de689572b5056d5a3107e1957
Adil Naseem <adilnaseemsheikh@gmail.com>
2023-05-15 20:20:49 +0500

    day 11 challenge added

---
+++ 30DaysJSChallenge/day11_sleep.js
@@ -0,0 +1,9 @@
+const sleep = async function (millis) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved");
+    }, millis);
+  });
+};
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t));
