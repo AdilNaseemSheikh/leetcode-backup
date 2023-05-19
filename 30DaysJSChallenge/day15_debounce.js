427d880186336cdb7f1fdacadb033148753f077b
Adil Naseem <adilnaseemsheikh@gmail.com>
2023-05-19 22:44:03 +0500

    code refactored for devounce

--- 30DaysJSChallenge/day15_debounce.js
+++ 30DaysJSChallenge/day15_debounce.js
@@ -2,22 +2,11 @@
 
 var debounce = function (fn, t) {
   let timer;
-  const setTimer = function (args) {
-    return setTimeout(() => {
-      console.log("executing fn");
-      fn(...args);
-      timer = null;
-    }, t);
-  };
   return function (...args) {
-    if (timer) {
-      clearTimeout(timer);
-      console.log("canceling timer and setting new one");
-      timer = setTimer(args);
-    } else {
-        console.log('Setting brand new Timer');
-      timer = setTimer(args);
-    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, t);
   };
 };
 
@@ -38,14 +27,3 @@ myFn().then((res) => {
   log("Hi there :) ");
   log("Hi there => ");
 });
-
-// setting brand new timer
-// canceling timer and setting new one
-// canceling timer and setting new one
-// executing fn
-// Hello World
-
-// setting brand new timer
-// canceling timer and setting new one
-// executing fn
-// Hi there =>
