ac94b3953a58485c6ccdcfc88caf8b3513edee72
Adil Naseem <adilnaseemsheikh@gmail.com>
2023-05-20 22:14:39 +0500

    day 16 challenge added

--- 30DaysJSChallenge/day16_throttle.js
+++ 30DaysJSChallenge/day16_throttle.js
@@ -1,54 +1,22 @@
-////////////////////
-
-// function throttle(cb, delay = 1000) {
-//   let shouldWait = false
-//   let waitingArgs
-//   const timeoutFunc = () => {
-//     if (waitingArgs == null) {
-//       shouldWait = false
-//     } else {
-//       cb(...waitingArgs)
-//       waitingArgs = null
-//       setTimeout(timeoutFunc, delay)
-//     }
-//   }
-
-//   return (...args) => {
-//     if (shouldWait) {
-//       waitingArgs = args
-//       return
-//     }
-
-//     cb(...args)
-//     shouldWait = true
-//     setTimeout(timeoutFunc, delay)
-//   }
-// }
-
-// /**
-//  * const throttled = throttle(console.log, 100);
-//  * throttled("log"); // logged immediately.
-//  * throttled("log"); // logged at t=100ms.
-//  */
-
-////////////////////
-
 var throttle = function (fn, t) {
   let timer = null;
   let currArg = null;
+  const timerFn = () => {
+    if (currArg) {
+      fn(...currArg);
+      timer = setTimeout(timerFn, t);
+      currArg = null;
+    } else {
+      timer = null;
+    }
+  };
 
   return function (...args) {
     if (timer) {
       currArg = args;
     } else {
       fn(...args);
-      timer = setTimeout(() => {
-        if (currArg) {
-          fn(...currArg);
-          currArg = null;
-        }
-        timer = null;
-      }, t);
+      timer = setTimeout(timerFn, t);
     }
   };
 };
